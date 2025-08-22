8efea9e9a982713ee2f5fea27b2e8e344079c53e
Marco200702 <mcordeirodegoes@gmail.com>
2025-08-21 22:29:28 -0300

    21/08/25

--- Etapa_5.c
+++ Etapa_5.c
@@ -1,22 +1,67 @@
 #include <stdio.h>
 int main(void){
 
-//ETAPA 5 - EXERCÍCIO --- Calculadora simples: Receba dois números e um operador (+, -, *, /) e execute a operação.
+//ETAPA 5 - EXERCÍCIO 13 --- Calculadora simples: Receba dois números e um operador (+, -, *, /) e execute a operação.
 
     float num1;
     float num2;
-    float result;
     char operador;
 
     printf("\nInsira o primeiro valor da conta: ");
-    scanf("%f", num1);
+    scanf("%f", &num1);
 
-    printf("\nInsira o primeiro valor da conta: ");
-    scanf("%c", operador);
+    printf("\nInsira o operador da conta (+ , - , * , /): ");
+    scanf(" %c", &operador);
 
     printf("\nInsira o segundo valor da conta: ");
-    scanf("%f", num2);
+    scanf("%f", &num2);
 
-    if ()
+    if (operador == '+'){
+        printf("\n%f + %f = %f\n\n", num1, num2, num1 + num2);
+    }
+
+    else if (operador == '-'){
+        printf("\n%f + %f = %f\n\n", num1, num2, num1 - num2);
+    }
+
+    else if (operador == '*'){
+        printf("\n%f + %f = %f\n\n", num1, num2, num1 * num2);
+    }
+
+    else if (operador == '/'){
+        printf("\n%f + %f = %f\n\n", num1, num2, num1 / num2);
+    }
+
+    else{
+        printf("O operador eh incondizente!");
+    }
+
+//# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+//ETAPA 5 - EXERCÍCIO 14 --- Conversor de temperaturas (C -> F) celsius para fahrenheit: escolha do tipo de conversão com if
+
+float graus;
+char tipo;
+float fahrenheit = 5/9;
+float result;
+
+printf("Insira a temperatura: ");
+scanf("%f", &graus);
+
+printf("Deseja transformar estes graus em celsius ou fahrenheit (c ou f): ");
+scanf(" %c", &tipo);
+
+if (tipo == 'c'){
+    result = (graus - 32) * fahrenheit;
+    printf("\n%f Celsius\n\n", result);
+}
+
+else if(tipo == 'f'){
+    result = (graus * fahrenheit) + 32;
+    printf("\n%f Fahrenheit\n\n", result);
+}
+else{
+    printf("O tipo de graus inserido não")
+}
 
 }
+
---
+++ Etapa_6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void){
+
+// ETAPA 6 - EXERCÍCIO 15 ---  Usando vetores (array), receba uma frase e retorne sua escrita inversa.
+
+char vetor [10];
+
+
+
+}
+
+//STR LEN
