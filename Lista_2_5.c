0516884c9514a9eb6d7d0648acc1cea80835f5e7
Marco200702 <mcordeirodegoes@gmail.com>
2025-09-18 22:23:19 -0300

    18/09/2025

---
+++ Lista_2_5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+//Escreva um procedimento que receba uma matriz quadrada 3x3 e preencha uma segunda matriz com a transposta dela.
+//Mostre as duas matrizes no main
+
+int matriz_transposta(matriz[3][3]){
+    for(int i = 0; )
+}
+
+int main (){
+int matriz[3][3];
+
+printf("\nInsira os valores da matriz:\n")
+for(int i = 0; i < 3; i++){
+    for(int j = 0; j < 3; j++){
+        printf("Posicao: [%d][%d]: ", i, j);
+    }
+}
+}
