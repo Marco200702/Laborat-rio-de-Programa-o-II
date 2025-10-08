45b677ca67bf4cfd5cbf5cd894693b008f88169c
Marco200702 <mcordeirodegoes@gmail.com>
2025-10-08 20:26:21 -0300

    08/10/2025

---
+++ trabalho_final.h
@@ -0,0 +1,10 @@
+#ifndef TRABALHOLIB_H
+#define TRABALHOLIB_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+//Funçoes de matemática básica
+int soma(int a, int b);
+int subtracao(int a, int b);
+int multiplicação (int a, int b);
